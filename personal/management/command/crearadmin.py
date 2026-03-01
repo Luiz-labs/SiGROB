ebcb193ad341fa942f1d20214d6c3b7203afc839
Luiz <luiz@mbp-de-luiz.lan>
2026-02-28 19:41:23 -0500

    Add Django command createadmin

--- personal/management/command/crearadmin.py
+++ personal/management/command/crearadmin.py
@@ -1,16 +1,19 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Crea usuario admin si no existe'
+    help = "Crea superusuario si no existe"
 
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email='admin@sigrob.com',
-                password='Admin123!'
-            )
-            self.stdout.write(self.style.SUCCESS('Admin creado'))
+        User = get_user_model()
+
+        username = "admin"
+        email = "admin@sigrob.com"
+        password = "Admin123!"
+
+        if not User.objects.filter(username=username).exists():
+            self.stdout.write("Creando superusuario...")
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(self.style.SUCCESS("Superusuario creado"))
         else:
-            self.stdout.write('Admin ya existe')
+            self.stdout.write("Superusuario ya existe")
